86c58a0fdc80bb98c278295da1cab94551622381
Virgold <virgoanchuks@gmail.com>
2023-09-19 11:14:45 +0100

    changed header to Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <p className="text-red-700">
       APP
-    </>
+    </p>
   )
 }
 
--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Header = () => {
+export const Navbar = () => {
   return (
     <div>Header</div>
   )
