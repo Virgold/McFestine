bcd61443bf9faef319a65fee71f4c59c41120565
Zubykings <zubykings@gmail.com>
2023-09-23 11:36:06 +0100

    "Footer"

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Footer } from "./components/common/Footer";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <p className="text-red-700">
-      APP
-    </p>
-  )
+    <>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
