db8de8bed17d0b709c9f538404f091c1bd1e0052
Zubykings <zubykings@gmail.com>
2023-09-28 12:05:01 +0100

    Footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -9,9 +9,9 @@ import McFestineIcon from "../../assets/images/McFestine_icon.png";
 export const Footer = () => {
   return (
     <div className="bg-[#313131] text-white footer mt-0">
-      <div className="container mx-auto py-8 px-4 md:px-0">
+      <div className="mx-auto py-8 px-4 lg:px-[70px] ">
         <div className=" flex flex-col lg:flex-row justify-between items-center pb-8">
-          <div className="md:w-2/3 lg:w-1/2 w-full flex flex-col justify-center items-center lg:items-start space-y-11 ">
+          <div className="md:w-2/3 lg:w-1/2 w-full flex flex-col justify-center items-center lg:items-start space-y-11 lg:me-[50px] ">
             <div className="mb-6 space-y-5 flex flex-col items-cemter justify-center ">
               <div className="space-y- flex flex-col items-center lg:items-start">
                 <img
@@ -31,16 +31,16 @@ export const Footer = () => {
               </div>
             </div>
 
-            <div className="md:h-[187px] lg:w-3/4 w-full  flex-shrink-0 rounded-e-[100px] opening-hours flex flex-col gap-5 md:gap-6 md:py-6 md:px-4 p-3 ">
-              <h2 className="text-2xl font-medium text-[#8C6321]">
+            <div className="md:h-[187px] w-full  flex-shrink-0 rounded-e-[100px] opening-hours flex flex-col gap-5 md:gap-6 md:py-6 md:px-4 p-3 ">
+              <h2 className="md:text-2xl text-xl  font-medium text-[#8C6321]">
                 Opening hours
               </h2>
-              <div className="flex md:gap-32 gap-20 md:text-xl text-lg items-center font-normal ">
+              <div className="flex md:gap-32 gap-5 lg:text-xl md:text-lg text-base items-center font-normal ">
                 <div className="">
                   <h4 className="text-[#8C6321] gap">Monday - Saturday</h4>
                   <span className="text-[#F5F5F5]">8am - 10pm</span>
                 </div>
-                <div className="">
+                <div className=" text-base md:text-lg lg:text-xl">
                   <h4 className="text-[#8C6321]">Sunday</h4>
                   <span className="text-[#F5F5F5]">9am - 10pm</span>
                 </div>
@@ -48,41 +48,41 @@ export const Footer = () => {
             </div>
           </div>
 
-          <div className="flex flex-col mt-14 lg:mt-0  lg:space-y-28 space-y-14 md:w-2/3 lg:w-1/2 border ">
-            <div className="flex md:flex-row flex-col border text-[#A3A3A3] w-full gap-14 shrink-0 ">
-              <ul className="flex flex-col w-full md:items-start items-center gap-3">
-                <li className="hover:bg-[#525252] px-3 py-1 rounded-md cursor-pointer">
+          <div className="flex flex-col mt-14 lg:mt-0 w-full space-y-14 md:w-2/3 lg:w-1/2 ">
+            <div className="flex md:flex-row flex-col text-[#A3A3A3] w-full gap-14 md:gap-0 items-center shrink-0 ">
+              <ul className="flex flex-col w-full items-start gap-3">
+                <li className="hover:bg-[#525252] w-full px-4 py-1 w-full rounded-md cursor-pointer">
                   <a href="">About Us</a>
                 </li>
-                <li className="hover:bg-[#525252] px-4 py-1 rounded-md cursor-pointer">
+                <li className="hover:bg-[#525252] px-4 py-1 w-full rounded-md cursor-pointer">
                   <a href="">Latest News</a>
                 </li>
-                <li className="hover:bg-[#525252] px-4 py-1 rounded-md cursor-pointer">
+                <li className="hover:bg-[#525252] px-4 py-1 w-full rounded-md cursor-pointer">
                   <a href="">Blog</a>
                 </li>
               </ul>
-              <ul className="flex flex-col w-full md:items-start items-center gap-3">
-                <li className="hover:bg-[#525252] px-4 py-1 rounded-md cursor-pointer">
+              <ul className="flex flex-col w-full items-start gap-3">
+                <li className="hover:bg-[#525252] px-4 py-1 w-full rounded-md cursor-pointer">
                   <a href="">Services</a>
                 </li>
-                <li className="hover:bg-[#525252] px-4 py-1 rounded-md cursor-pointer">
+                <li className="hover:bg-[#525252] px-4 py-1 w-full rounded-md cursor-pointer">
                   <a href="">Menu</a>
                 </li>
-                <li className="hover:bg-[#525252] px-4 py-1 rounded-md cursor-pointer">
+                <li className="hover:bg-[#525252] px-4 py-1 w-full rounded-md cursor-pointer">
                   <a href="">Store</a>
                 </li>
               </ul>
-              <ul className="flex flex-col md:items-start items-center w-full gap-3">
-                <li className="hover:bg-[#525252] px-4 py-1 rounded-md cursor-pointer">
+              <ul className="flex flex-col items-start w-full gap-3">
+                <li className="hover:bg-[#525252] px-4 py-1 w-full rounded-md cursor-pointer">
                   <a href="">Contact Us</a>
                 </li>
-                <li className="hover:bg-[#525252] px-4 py-1 rounded-md cursor-pointer">
+                <li className="hover:bg-[#525252] px-4 py-1 w-full rounded-md cursor-pointer">
                   <a href="">FAQ</a>
                 </li>
               </ul>
             </div>
 
-            <div className="mt-6 flex border flex-col w-full text-[#A3A3A3] text-center lg:text-right lg:items-end font-semibold py-4 space-y-4">
+            <div className="mt-6 flex flex-col w-full text-[#A3A3A3] text-center lg:text-right lg:items-end font-semibold py-4 space-y-4">
               <strong className="md:text-3xl text-2xl block mb-2 ">
                 NewsLetter
               </strong>
