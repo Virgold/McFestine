f9642f9c52a58e9cda7189c75970fa4fdb0ffed9
Virgold <virgoanchuks@gmail.com>
2023-09-28 01:23:37 +0100

    resolving merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-<<<<<<< HEAD
-import HomePage from "./components/hompage/HomePage";
-=======
 import HomePage from "./pages/HomePage";
->>>>>>> 48f55b9f6a11cfa1c6ce688af7f4701a45ca027e
 
 function App() {
   const [count, setCount] = useState(0);
--- src/components/hompage/HomePage.jsx
+++ src/components/hompage/HomePage.jsx
@@ -4,17 +4,11 @@ import { Footer } from '../common/Footer'
 
 const HomePage = () => {
     return (
-<<<<<<< HEAD
-        <div className=" h-[1000000px]">
-            <Navbar />
-            <Footer />
-=======
         <div className="">
             <Navbar
                 loginStyle='text-[white] bg-[#CF9832] border border-[white] rounded-full md:py-[12px] py-[5px] md:px-[17px] px-[6px]'
                 signUpStyle='text-[#CF9832] bg-white border border-[white] shadow rounded-full md:py-[12px] py-[5px] md:px-[17px] px-[6px]'
             />
->>>>>>> 48f55b9f6a11cfa1c6ce688af7f4701a45ca027e
         </div>
     )
 }
