2509a9fadedb7ef748a43ed3c837b4bdaf812f66
Esvicvictor <esvicvi@gmail.com>
2023-10-10 01:06:43 +0100

    worked on the hero secion

---
+++ src/components/hompage/Hero.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Hero.css";
+import Button from "../common/Button";
+import foodimage from "../../assets/images/herofoodimage.png";
+import threedot from "../../assets/images/threedots.png";
+import rightarrow from "../../assets/images/arrow_right_alt.png";
+import cart from "../../assets/images/cartheroimage.png"
+
+const Hero = () => {
+  return (
+    <div>
+      <section class='heroSection'>
+        <div class='slantBackground'>
+          <div class='enclosingSection'>
+            <div class='textHero'>
+              <h1>
+                Welcome to <span>McFESTINE’S</span>
+              </h1>
+              <h3>Restaurant & Supermarket</h3>
+              <p>
+                Indulge in the best of the Season at <span>Mcfestines,</span> whatever you’re
+                preparing or preparing for. We can help you make it great!
+              </p>
+              <img src={rightarrow} alt="Arrow"/>
+              <Button
+              
+                text='Get started'
+                styles={`text-[white] lg:w-[30%] md:w-[40%] text-[13px] lg:text-[21px] md:text-[13px]  bg-[#CF9832] border border-[#CF9832] rounded-full md:py-[8px] py-[8px] md:px-[0px] px-[25px]`}
+              />
+            </div>
+            <div class='imageHeroSection'>
+              <div class='imageOne'>
+                <img src={foodimage} alt='Food served in a plate' />
+              </div>
+              <div class='imageTwo'></div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className='heroBottom'>
+        <div class='dots'>
+         <img src={threedot} alt="" />
+        </div>
+        <div className='heroCart'>
+          <img src={cart} alt='Cart icon' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/hompage/HomePage.jsx
+++ src/components/hompage/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Navbar } from '../common/Navbar'
 import { Footer } from '../common/Footer'
 import AboutUs from '../about/AboutUs'
+import Hero from './Hero'
 
 const HomePage = () => {
     return (
@@ -10,6 +11,7 @@ const HomePage = () => {
                 loginStyle='text-[white] bg-[#CF9832] border border-[white] rounded-full md:py-[12px] py-[5px] md:px-[17px] px-[6px]'
                 signUpStyle='text-[#CF9832] bg-white border border-[white] shadow rounded-full md:py-[12px] py-[5px] md:px-[17px] px-[6px]'
             />
+            <Hero/>
             <AboutUs/>
             <Footer />
         </div>
